3e760eec51ffb5ecba31ad3400dc2d0b8a72d0bb
mpachecoy <ing.pachecoymartin@gmail.com>
2026-03-16 19:58:16 -0300

    Integracios de router y controller

--- modulo-4/app.js
+++ modulo-4/app.js
@@ -1,20 +1,11 @@
 import express from 'express';
+import productRouter from './routes/router.js'
 
 const app = express();
-
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    const mensaje = { mensaje: "Hola mundo!"}
-    res.json({ mensaje, status: 'ok' })
-});
+app.use('/api/products', productRouter);
 
-
-app.post('/echo', (req, res) => {
-    const body = req.body;
-
-    res.json({ mensaje: " Datos recibidos ", satatus: 'ok', datos: body })
-});
 
 const PORT = 3000;
 app.listen(PORT, () => {
---
+++ modulo-4/routes/router.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import productController from "../controllers /controller.js"
+
+const router = Router();
+
+router.get('/', productController.getProduct);
+router.get('/:id', productController.getProductById);
+router.post('/', productController.createProduct);
+router.put('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
+
+export default router;
